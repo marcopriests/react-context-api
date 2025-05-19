d91618ada2fa3079b5dee1aa2207db4f2f394be4
marcopriests <prtmrc98@gmail.com>
2025-05-19 15:45:36 +0200

    [UPD] impostato PostsPage

--- src/Pages/PostsPage.jsx
+++ src/Pages/PostsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PostsList from '../Components/PostsList'
 
-const PostsPage = () => {
+const PostsPage = ({ posts }) => {
     return (
-        <div>
-
+        <div className='container p-5'>
+            <h1>Posts</h1>
+            <PostsList posts={posts} />
         </div>
     )
 }
